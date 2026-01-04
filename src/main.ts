37ed1edd328c712336fd36b2b37af189f45cfe12
oceanep <oeverspe@gmail.com>
2026-01-04 22:47:58 +0900

    added translation for z and rotation for xz plane

--- src/main.ts
+++ src/main.ts
@@ -12,18 +12,30 @@ const ctx = game.getContext('2d') as CanvasRenderingContext2D
 
 console.log(ctx)
 
+interface vec2 {
+    x: number,
+    y: number
+}
+
+interface vec3 extends vec2 {
+    z: number
+}
+
+
 function clear() {
     ctx.fillStyle = BACKGROUND
     ctx.fillRect(0, 0, game.width, game.height)
 }
 
-function point({ x, y }: { x: number, y: number }) {
-    const s: number = 20
+// Renders point
+function point({ x, y }: vec2): void {
+    const s: number = 10
     ctx.fillStyle = FOREGROUND
     ctx.fillRect(x - s / 2, y - s / 2, s, s)
 }
 
-function project(p: { x: number, y: number }) {
+// Calculates where to play point on screen in 2D
+function screen(p: vec2): vec2 {
     // -1..1 => 0..2 => 0..1 => 0..w
     // 1 - (-1..1 => 0..2 => 0..1) => 0..h
     return {
@@ -32,5 +44,58 @@ function project(p: { x: number, y: number }) {
     }
 }
 
-clear()
-point(project({ x: 0, y: 0 }))
+// Translates 3D space coordinate into 2D space
+function project({ x, y, z }: vec3): vec2 {
+    return {
+        x: x / z,
+        y: y / z,
+    }
+}
+
+function translateZ({ x, y, z }: vec3, dz: number): vec3 {
+    return { x, y, z: z + dz }
+}
+
+// rotate on y axis (xz)
+// x' = xcos(ang) - ysin(ang)
+// y' = xsin(ang) + ycos(ang)
+function rotateXZ({ x, y, z }: vec3, angle: number): vec3 {
+    const c = Math.cos(angle)
+    const s = Math.sin(angle)
+
+    return {
+        x: x * c - z * s,
+        y,
+        z: x * s + z * c
+    }
+}
+
+const vecs: vec3[] = [
+    { x: 0.25, y: 0.25, z: 0.25 },
+    { x: -0.25, y: 0.25, z: 0.25 },
+    { x: 0.25, y: -0.25, z: 0.25 },
+    { x: -0.25, y: -0.25, z: 0.25 },
+
+    { x: 0.25, y: 0.25, z: -0.25 },
+    { x: -0.25, y: 0.25, z: -0.25 },
+    { x: 0.25, y: -0.25, z: -0.25 },
+    { x: -0.25, y: -0.25, z: -0.25 },
+]
+
+const FPS = 60
+let dz = .7
+let angle = 0
+
+function frame(): void {
+    const dt = 1 / FPS
+    //when 2 seconds pass stop animation for delta z
+    dz += dz > 120 * dt ? 0 : 1 * dt
+    angle += Math.PI * dt
+    clear()
+    for (const v of vecs) {
+        point(screen(project(translateZ(rotateXZ(v, angle), dz))))
+    }
+    setTimeout(frame, 1000 / FPS)
+}
+
+setTimeout(frame, 1000 / FPS)
